4583f4609f4daadd9dd95346dd178751c4212dcd
Nadav Shaar <nadavshaar@gmail.com>
2020-10-10 21:51:08 +0300

    first-commit.

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import './index.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1>React-App</h1>
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
