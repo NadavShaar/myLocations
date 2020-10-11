a0f1a25cd69a90e09fe2bbbf2036c45392273bfa
Nadav Shaar <nadavshaar@gmail.com>
2020-10-11 07:32:49 +0300

    update.

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,55 @@
 const path = require('path');
-const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.jsx',
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        chunkFilename: '[id].js',
-        publicPath: '/'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.jsx$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/,
-                loader: 'url-loader?limit=10000&name=img/[name].[ext]'
-            }
+module.exports = (env, argv) => {
+
+    const devMode = argv.mode !== 'production';
+
+    return {
+        entry: './src/index.jsx',
+        output:{
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+            chunkFilename: '[id].js',
+            // publicPath: '/'
+        },
+        devtool: devMode ? 'inline-source-map' : false,
+        resolve: {
+            extensions: ['.js', '.jsx']
+        },
+        mode: argv.mode,
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.jsx$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(png|jpe?g|gif)$/,
+                    loader: 'url-loader?limit=10000&name=img/[name].[ext]'
+                }
+            ]
+        },
+        devServer: {
+            historyApiFallback: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: __dirname + '/src/index.html',
+                filename: 'index.html',
+                inject: 'body'
+            })
         ]
-    },
-    devServer: {
-        historyApiFallback: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        })
-    ]
+    }
 
 };
